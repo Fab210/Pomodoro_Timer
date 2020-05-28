ce55c0abb00d941a31dc91c845529130dd5c3b09
fabio <fabio@W-FASA-01>
2020-05-28 16:09:59 +0200

    Updates

---
+++ precache-manifest.0f882f30259ac633b673a99a2f411314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79625becc49a329bd004dec8fd36323",
+    "url": "/Pomodoro_Timer/index.html"
+  },
+  {
+    "revision": "457c0965e427c9e2452e",
+    "url": "/Pomodoro_Timer/static/css/main.9830fa6f.chunk.css"
+  },
+  {
+    "revision": "2796c7902ffc7cc90655",
+    "url": "/Pomodoro_Timer/static/js/2.57018d4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro_Timer/static/js/2.57018d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457c0965e427c9e2452e",
+    "url": "/Pomodoro_Timer/static/js/main.12a1d174.chunk.js"
+  },
+  {
+    "revision": "d7bff88feab21d40d800",
+    "url": "/Pomodoro_Timer/static/js/runtime-main.3997fa5d.js"
+  }
+]);
